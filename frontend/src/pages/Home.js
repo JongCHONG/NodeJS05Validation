50a2b235778567103193d13b93c448741b9f242b
JongCHONG <yonhoa27@hotmail.fr>
2021-12-16 11:27:03 +0100

    update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   const [users, setUsers] = useState(null)
@@ -9,10 +10,23 @@ const Home = () => {
       .then(data => setUsers(data))
   }, [])
 
+  if (!users) {
+    return <h1>Chargement...</h1>
+  }
+
   console.log(users)
   return (
     <div>
-      Home
+      <Link to="/formuser">New user</Link>
+      {users.map(element => {
+        return (
+          <p key={element.name}>
+            <Link to={`/users/${element.slug}`}>
+              {element.name}
+            </Link>
+          </p>
+        )
+      })}
     </div>
   )
 }
--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const User = () => {
+  const { slug } = useParams()
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${slug}`)
+    .then(response => response.json())
+    .then(data => setUser(data))
+  }, [])
+
+  if (!user) {
+    return <h1>Chargement...</h1>
+  }
+
+  console.log(user)
   return (
     <div>
-      User
+      <p>{user.name}</p>
+      <p>{user.city}</p>
+      <p>{user.profile_picture}</p>
     </div>
   )
 }
